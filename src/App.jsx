116d17700c078baa35f4ef7c944580eb94585a4a
Om Jaiswal <jom15718@gmail.com>
2026-02-04 15:38:24 +0530

    Refactor App component to include Hero and Second components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./Componets/Navbar";
-function App(){
-  return(
-    <Navbar/>
-  )
+import Hero from "./Componets/Hero";
+import Second from "./Componets/Second";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Second />
+    </div>
+  );
 }
 export default App;
