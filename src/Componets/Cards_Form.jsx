285a2675c72dca08ddb59c594743ee00bb212567
Om Jaiswal <jom15718@gmail.com>
2026-02-21 16:57:19 +0530

    Refactor Cards_Form component to accept users prop and render Card_form for each user

--- src/Componets/Cards_Form.jsx
+++ src/Componets/Cards_Form.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Card_form from './Card_form'
 
-function Cards_Form() {
+function Cards_Form({users}) {
   return (
-    <div className='w-full h-[60vh] bg-sky-100'>Cards_Form</div>
+    <div className='w-full h-96 max-h-96 overflow-auto flex justify-center gap-4 p-4 flex-wrap'>
+        {users.map((item,index)=>{
+         return<Card_form key={index} users={users}/>
+        })}
+     
+    </div>
   )
 }
 
